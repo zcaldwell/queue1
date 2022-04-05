95a255fbfcdfb6394c0a1b1947f5319c761fb7b2
zcaldwell <mintyst.john@gmail.com>
2022-04-05 14:45:21 -0700

    first two done

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ class Stack {
   peek() {
     return this.items[this.items.length - 1];
   }
+
+  get size() {
+    return this.items.length;
+  }
 }
 
 const stack = new Stack();
@@ -29,3 +33,37 @@ console.log(stack.pop()); // 'llama'
 console.log(stack.peek()); // 'fox'
 console.log(stack.pop()); // 'fox'
 console.log(stack.pop()); // null
+
+class Queue {
+  #list = [];
+  #index = 0;
+
+  enqueue(item) {
+    this.#list.push(item);
+  }
+
+  dequeue() {
+    const item = this.#list[this.#index];
+    if (!this.hasNext()) return null;
+    this.#index++;
+    return item;
+  }
+
+  hasNext() {
+    return !!(this.#list.length - this.#index);
+  }
+}
+
+const queue = new Queue();
+queue.enqueue("fox");
+queue.enqueue("goose");
+queue.enqueue("lizard");
+console.log(queue.dequeue()); // 'fox'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue("llama");
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // false
+console.log(queue.dequeue()); // null
